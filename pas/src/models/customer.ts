b54a5e5072cd077d8da97832635a4325897492de
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-06-15 09:50:32 -0700

    update customer model to include prizes, token, and lastPlay

--- pas/src/models/customer.ts
+++ pas/src/models/customer.ts
@@ -3,5 +3,13 @@ export class Customer {
     public currentPrize: string;
     public contractorId: string;
     //will need to create a "Prize" class to reference here (or just create it as part of the Customer model), in order to create an array of those Prizes
-    public prizes = [];
+    public prizes = [
+        [1, false], [2, false], [3, false], [4, false],
+        [5, false], [6, false], [7, false], [8, false],
+        [9, false], [10, false], [11, false], [12, false],
+        [13, false], [14, false], [15, false], [16, false]
+    ];
+    public token: string;
+    public lastPlay: Date;
 }
+
